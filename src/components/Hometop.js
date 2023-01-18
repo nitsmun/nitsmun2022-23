593d8b165a25c8febdb34267818c62070b7e5b96
hereisadi <hereisadi@proton.me>
2023-01-18 22:16:24 +0530

    ADD:exe board reg link

--- src/components/Hometop.js
+++ src/components/Hometop.js
@@ -8,26 +8,25 @@ const Hometop = () => {
         <div className="hometop-img">
           <img src="./Images/nitsmun-logo.svg" alt="NITSMUN" />
         </div>
-
-       
       </div>
 
-    
       <div className="home-wrapped">
-      <Link to='/wrapped'>
-            <img src="./Images/wrapped2022/wrapping up 2022 button.png" alt="Wrapped 2022" />
-          </Link>
-        </div>
-
+        <Link to="/wrapped">
+          <img
+            src="./Images/wrapped2022/wrapping up 2022 button.png"
+            alt="Wrapped 2022"
+          />
+        </Link>
+      </div>
 
       <div className="hometop-apply">
         <div className="hometop-reg">
           <ul style={{ color: "white" }}>
-            Mock Mun 2023{" "}
-            <span style={{ color: "rgb(45, 210, 255)" }}>
-              Registration
-            </span>{" "}
-            Is Now Open!<img className="tnew-gif" src="./Images/tnew.gif" alt="" />
+            <span style={{ color: "rgb(45, 210, 255)" }}>Mock Mun 2023</span>{" "}
+            And <span style={{ color: "rgb(45, 210, 255)" }}>Executive Board </span>
+            Registration Is
+            Now Open!
+            <img className="tnew-gif" src="./Images/tnew.gif" alt="" />
           </ul>
         </div>
         <div className="hometop-link1">
@@ -36,8 +35,6 @@ const Hometop = () => {
           </Link>
         </div>
       </div>
-
-
     </div>
   );
 };
