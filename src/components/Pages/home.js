f1fbc00e6e55e45e099b51057d94359fac14dcb7
hereisadi <hereisadi@proton.me>
2023-01-30 21:14:17 +0530

    ADD:loader

--- src/components/Pages/home.js
+++ src/components/Pages/home.js
@@ -5,23 +5,23 @@ import { TabTitle } from "../Utils/Genfn";
 import Secretary from "../Secretary";
 import Hometop from "../Hometop";
 import ArticleHome2 from "../ArticleHome2";
-import Loading from "../Loading";
+// import Loading from "../Loading";
 import ImageSliderComponent from "../ImageSliderComponent";
 import Underline from "../Underline";
 
 const Home = () => {
   TabTitle("NITS MUN");
-  const [isFetching, setIsFetching] = useState(true);
+  // const [isFetching, setIsFetching] = useState(true);
 
-  useEffect(() => {
-    setTimeout(function () {
-      setIsFetching(false);       //setting up "fetching data loader"
-    }, 150);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(function () {
+  //     setIsFetching(false);       //setting up "fetching data loader"
+  //   }, 150);
+  // }, []);
 
-  if (isFetching) {
-    return <Loading />;
-  }
+  // if (isFetching) {
+  //   return <Loading />;
+  // }
 
   return (
     <>
