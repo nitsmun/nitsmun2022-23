83d0ff7fb165b42731a2041fd4f817e653eb0e33
hereisadi <hereisadi@proton.me>
2023-02-01 23:18:35 +0530

    UPD:update linktree

--- src/components/Hometop.js
+++ src/components/Hometop.js
@@ -43,12 +43,12 @@ const Hometop = () => {
         </Link>
       </div>
 
-      {/* <div className="hometop-apply">
+      <div className="hometop-apply">
         <div className="hometop-reg">
           <ul style={{ color: "white" }}>
-            <span style={{ color: "rgb(45, 210, 255)" }} className="home-bner">Mock Mun 2023</span>{" "}
-            And{" "}
-            <span style={{ color: "rgb(45, 210, 255)" }} className="home-bner">Executive Board </span>
+            <span style={{ color: "rgb(45, 210, 255)" }} className="home-bner">Nitsmun Annual Conference 2023</span>{" "}
+            {/* And{" "}
+            <span style={{ color: "rgb(45, 210, 255)" }} className="home-bner">Executive Board </span> */}
             Registration Is Now Open!
             <img className="tnew-gif" src="./Images/tnew.gif" alt="" />
           </ul>
@@ -58,7 +58,7 @@ const Hometop = () => {
             <button className="hometop-link1btn">Register Now!</button>
           </Link>
         </div>
-      </div> */}
+      </div>
 
       <div className="annual-2023-poster">
         <img
--- src/components/Pages/Nitsmun2023/Nitsmun2023.jsx
+++ src/components/Pages/Nitsmun2023/Nitsmun2023.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Nitsmun2023.css'
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
+import { Link } from 'react-router-dom';
 const Nitsmun2023 = () => {
     useEffect(() => {
         document.title = 'NITSMUN 2023 | NITSMUN';
@@ -38,7 +39,7 @@ const Nitsmun2023 = () => {
                     <h2>Do you like to think critically and imagine yourself in the shoes of world leaders? Do you feel that you can form strong blogs and tilt the final decision in your interest? Do you have enough knowledge about the world that embraces wizards and magic? If yes, <span style={{ fontFamily: "Nunito", fontStyle: "italic" }}>NITSMUN, 2023</span> is surely for you!<br /><br />After two continuous years of online conferences, this time <span style={{ fontWeight: "900", fontFamily: "nunito" }}>NITSMUN</span> is all set to organise its annual conference offline, under the NITSMUN module of     <span style={{ fontFamily: "Nunito", fontStyle: "italic" }}>Incandescence, 2023</span>. With the theme of <span style={{ fontStyle: 'italic', fontFamily: 'nunito', fontWeight: '900', color: "#794894" }}>"breaking free"</span>, Incandescence, this year, comes in with the promise of being one of its kind. This cultural fest of <span style={{ fontFamily: "Nunito", fontStyle: "italic" }}>NIT, Silchar</span> has always witnessed numerous talents from all over the country. Being one of the best cultural fests of the Northeast, this time too Incandescence promises to keep up to its legacy and "break free" from the normal, you have always seen.<br /><br />Going with the promise made by Incandescence, NITSMUN too has made it a point to make its annual conference different from the past years. The conference will be held from <span style={{ fontFamily: 'nunito', fontWeight: '900' }}>17<sup style={{ fontWeight: "900" }}>th</sup> February 2023</span> to <span style={{ fontFamily: 'nunito', fontWeight: '900' }}>19<sup style={{ fontWeight: "900" }}>th</sup> February 2023</span>.</h2>
                 </div>
 
-                <div className='off-reg-msg'>UPD: NITSMUN 2023 registration form will be released soon!</div>
+                {/* <div className='off-reg-msg'>UPD: NITSMUN 2023 registration form will be released soon!</div> */}
 
                 <div className="bg-pvd"></div>
                 <div className="comt-sn" id="comt-23">
@@ -63,8 +64,8 @@ const Nitsmun2023 = () => {
                             <div className="rgster-nts23-rd-mr">
                                 <div className="rgster-nts23">
 
-                                    {/* <Link to="#">       <button style={{ cursor: "pointer" }}>Register Now!</button></Link> */}
-                                    <button style={{ cursor: 'not-allowed' }}>Register Now!</button>
+                                    <Link to="/apply"><button style={{ cursor: "pointer" }}>Register Now!</button></Link>
+                                    {/* <button style={{ cursor: 'not-allowed' }}>Register Now!</button> */}
                                 </div>
                                 <div className="rd-mr-0">
                                     <button onClick={onOpenModal}>Read More</button>
@@ -89,11 +90,11 @@ const Nitsmun2023 = () => {
                                             </h2>
                                         </div>
 
-                                        <div className='off-reg-msg cntr-rd-mr-mg'>UPD: NITSMUN 2023 registration form and Background guide will be released soon!</div>
+                                        <div className='off-reg-msg cntr-rd-mr-mg'>UPD:Background guide will be released soon!</div>
 
                                         <div className="btn-rd-mr">
                                             <div className="rgster-nts23">
-                                                <button style={{ cursor: 'not-allowed' }}>Register Now!</button>
+                                                <Link to="/apply"><button style={{ cursor: "pointer" }}>Register Now!</button></Link>
                                             </div>
 
                                             <div className="rgster-nts23">
@@ -122,9 +123,7 @@ const Nitsmun2023 = () => {
 
                             <div className="rgster-nts23-rd-mr">
                                 <div className="rgster-nts23">
-                                    {/* <Link to="#">       <button style={{ cursor: "pointer" }}>Register Now!</button></Link> */}
-
-                                    <button style={{ cursor: 'not-allowed' }}>Register Now!</button>
+                                    <Link to="/apply"><button style={{ cursor: "pointer" }}>Register Now!</button></Link>
                                 </div>
 
                                 <div className="rd-mr-0">
@@ -141,21 +140,17 @@ const Nitsmun2023 = () => {
                                             <img src="https://www.linkpicture.com/q/ipc-min.jpg" alt="" />
                                         </div>
 
-                                        {/* <div className="rd-mr-agnd">
-                                            <h2> AGENDA- "Prosecuting Mr. Harry James Potter for destroying the elder wand"</h2>
-                                        </div> */}
-
                                         <div className="rd-mr-cont">
                                             <h2>
                                                 Do you want to step in the shoes of the journalists, who work day and night to bring out information about the world? Do you feel you can hold press conferences and question world leaders? If yes, then IPC is the right place for you. The delegates under IPC keep a note of the happenings of a particular committee and question the other delegates under that committee from time to time when the need arises. As they say, the pen is mightier than the sword, your pen might help to become the best journalist at the conference. So, join in, show the power of your pen and win the title.
                                             </h2>
                                         </div>
 
-                                        <div className='off-reg-msg cntr-rd-mr-mg'>UPD: NITSMUN 2023 registration form and Background guide will be released soon!</div>
+                                        <div className='off-reg-msg cntr-rd-mr-mg'>UPD:Background guide will be released soon!</div>
 
                                         <div className="btn-rd-mr">
                                             <div className="rgster-nts23">
-                                                <button style={{ cursor: 'not-allowed' }}>Register Now!</button>
+                                                <Link to="/apply"><button style={{ cursor: "pointer" }}>Register Now!</button></Link>
                                             </div>
 
                                             <div className="rgster-nts23">
@@ -183,9 +178,7 @@ const Nitsmun2023 = () => {
 
                             <div className="rgster-nts23-rd-mr">
                                 <div className="rgster-nts23">
-                                    {/* <Link to="#">       <button style={{ cursor:'not-allowed' }}>Register Now!</button></Link> */}
-
-                                    <button style={{ cursor: 'not-allowed' }}>Register Now!</button>
+                                    <Link to="/apply"><button style={{ cursor: "pointer" }}>Register Now!</button></Link>
                                 </div>
                                 <div className="rd-mr-0">
                                     <button onClick={onOpenModalnato}>Read More</button>
@@ -211,11 +204,11 @@ const Nitsmun2023 = () => {
                                             </h2>
                                         </div>
 
-                                        <div className='off-reg-msg cntr-rd-mr-mg'>UPD: NITSMUN 2023 registration form and Background guide will be released soon!</div>
+                                        <div className='off-reg-msg cntr-rd-mr-mg'>UPD:Background guide will be released soon!</div>
 
                                         <div className="btn-rd-mr">
                                             <div className="rgster-nts23">
-                                                <button style={{ cursor: 'not-allowed' }}>Register Now!</button>
+                                                <Link to="/apply"><button style={{ cursor: "pointer" }}>Register Now!</button></Link>
                                             </div>
 
                                             <div className="rgster-nts23">
