aca5e6ea7beb4dfa4e5fa93fff7ee7dc41af99b7
hereisadi <hereisadi@proton.me>
2023-02-01 12:15:07 +0530

    ADD:btns in nitsmun2k23

--- src/components/Pages/Nitsmun2023/Nitsmun2023.jsx
+++ src/components/Pages/Nitsmun2023/Nitsmun2023.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import './Nitsmun2023.css'
+import { Link } from 'react-router-dom';
 const Nitsmun2023 = () => {
     useEffect(() => {
         document.title = 'NITSMUN 2023 | NITSMUN';
@@ -21,7 +22,7 @@ const Nitsmun2023 = () => {
                     <h2>Do you like to think critically and imagine yourself in the shoes of world leaders? Do you feel that you can form strong blogs and tilt the final decision in your interest? Do you have enough knowledge about the world that embraces wizards and magic? If yes, <span style={{ fontFamily: "Nunito", fontStyle: "italic" }}>NITSMUN, 2023</span> is surely for you!<br /><br />After two continuous years of online conferences, this time <span style={{ fontWeight: "900", fontFamily: "nunito" }}>NITSMUN</span> is all set to organise its annual conference offline, under the NITSMUN module of     <span style={{ fontFamily: "Nunito", fontStyle: "italic" }}>Incandescence, 2023</span>. With the theme of <span style={{ fontStyle: 'italic', fontFamily: 'nunito', fontWeight: '900', color: "#794894" }}>"breaking free"</span>, Incandescence, this year, comes in with the promise of being one of its kind. This cultural fest of <span style={{ fontFamily: "Nunito", fontStyle: "italic" }}>NIT, Silchar</span> has always witnessed numerous talents from all over the country. Being one of the best cultural fests of the Northeast, this time too Incandescence promises to keep up to its legacy and "break free" from the normal, you have always seen.<br /><br />Going with the promise made by Incandescence, NITSMUN too has made it a point to make its annual conference different from the past years. The conference will be held from <span style={{ fontFamily: 'nunito', fontWeight: '900' }}>17<sup style={{ fontWeight: "900" }}>th</sup> February 2023</span> to <span style={{ fontFamily: 'nunito', fontWeight: '900' }}>19<sup style={{ fontWeight: "900" }}>th</sup> February 2023</span>.</h2>
                 </div>
 
-
+                <div className='off-reg-msg'>UPD: NITSMUN 2023 registration form will be released soon!</div>
 
                 <div className="bg-pvd"></div>
                 <div className="comt-sn" id="comt-23">
@@ -35,32 +36,72 @@ const Nitsmun2023 = () => {
                                 <img src="https://www.linkpicture.com/q/icow-min.jpg" alt="" />
                             </div>
                             <div className="comt-txt-imp">
-                                <h2>Presenting to you, our first committee and also the one we are particularly the most thrilled for and for which we wish you would be equally excited, The International Confederation of Wizards!</h2>
+                                <h2>Presenting to you, our first committee and also the one we are particularly the most thrilled for and for which we wish you would be equally excited, <span style={{ fontWeight: '900', fontFamily: "Nunito" }}>The International Confederation of Wizards</span>!</h2>
+                            </div>
+
+                            <div className="rgster-nts23-rd-mr">
+                                <div className="rgster-nts23">
+                                    {/* <Link to="#">       <button style={{ cursor: "pointer" }}>Register Now!</button></Link> */}
+
+                                    <button style={{ cursor: 'not-allowed' }}>Register Now!</button>
+                                </div>
+
+
+
+                                <div className="rd-mr-0">
+                                    <button>Read More</button>
+                                </div>
                             </div>
                         </div>
-                        <div className="agnd">
-                            <div className="img-nts23">
-                                <img src="https://www.linkpicture.com/q/nato-min.jpg" alt="" />
-                            </div>
-                            <div className="comt-txt-imp">
-                                <h2>Presenting to you our third Committee :North Atlantic Treaty Organzation( NATO): The NATO having been established in 1949 has since been serving its task of protecting the states of Europe, facilitate cooperation among states and secure peace in the continent.</h2>
-                            </div>
-                        </div>
+
                         <div className="agnd">
                             <div className="img-nts23">
                                 <img src="https://www.linkpicture.com/q/ipc-min.jpg" alt="" />
                             </div>
                             <div className="comt-txt-imp">
-                                <h2>Our second committee is for all the journalism enthusiasts out there, the International Press Corps.</h2>
+                                <h2>Our second committee is for all the journalism enthusiasts out there, the <span style={{ fontWeight: '900', fontFamily: "Nunito" }}>International Press Corps</span>.</h2>
+                            </div>
+
+                            <div className="rgster-nts23-rd-mr">
+                                <div className="rgster-nts23">
+                                    {/* <Link to="#">       <button style={{ cursor: "pointer" }}>Register Now!</button></Link> */}
+
+                                    <button style={{ cursor: 'not-allowed' }}>Register Now!</button>
+                                </div>
+
+                                <div className="rd-mr-0">
+                                    <button>Read More</button>
+                                </div>
                             </div>
                         </div>
+
+                        <div className="agnd">
+                            <div className="img-nts23">
+                                <img src="https://www.linkpicture.com/q/nato-min.jpg" alt="" />
+                            </div>
+                            <div className="comt-txt-imp">
+                                <h2>Presenting to you our third Committee:  <span style={{ fontWeight: '900', fontFamily: "Nunito" }}>North Atlantic Treaty Organzation</span> (NATO): The <span style={{ fontWeight: '900', fontFamily: "Nunito" }}>NATO</span> having been established in 1949 has since been serving its task of protecting the states of Europe, facilitate cooperation among states and secure peace in the continent.</h2>
+                            </div>
+
+                            <div className="rgster-nts23-rd-mr">
+                                <div className="rgster-nts23">
+                                    {/* <Link to="#">       <button style={{ cursor:'not-allowed' }}>Register Now!</button></Link> */}
+
+                                    <button style={{ cursor: 'not-allowed' }}>Register Now!</button>
+                                </div>
+                                <div className="rd-mr-0">
+                                    <button>Read More</button>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
-                <div className='off-reg-msg'>UPD: NITSMUN 2023 registration form will be released soon!</div>
 
+                {/* 
                 <div className="link-btn-btm">
                     <p className='p-by mock23-txt' style={{ cursor: 'not-allowed' }}>Click here to register yourself in NITSMUN 2023!</p>
-                </div>
+                </div> */}
             </div>
         </div>
 
