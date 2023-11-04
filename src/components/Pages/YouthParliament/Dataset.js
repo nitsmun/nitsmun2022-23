ff0eb6089d6a06cf20b0d4f3a57309bfbd4a3bcb
hereisadi <hereisadi@proton.me>
2023-11-04 15:16:24 +0530

    FEAT:Youth parliament

---
+++ src/components/Pages/YouthParliament/Dataset.js
@@ -0,0 +1,109 @@
+export const FaqData = [
+  {
+    question: "What is NITSMUN?",
+    answer:
+      "NITSMUN is a club at NIT Silchar that, since its inception, has been dedicated to fostering a culture of Model United Nations (MUNs) on campus.",
+  },
+  {
+    question: "What is Youth Parliament (YPP)?",
+    answer:
+      "Youth Parliament (YPP) is a program that offers young people a platform to participate in democratic processes, providing them with a mock parliamentary setting and exposure to parliamentary debates.",
+  },
+  {
+    question: "What is Lok Sabha?",
+    answer:
+      "The Lok Sabha, also known as the House of the People, is the lower house of India's bicameral Parliament, with the Rajya Sabha serving as the upper house.",
+  },
+  {
+    question: "When will NITSMUN organise YP?",
+    answer: "NITSMUN will organise Youth Parliament on November 5, 2023",
+  },
+  {
+    question: "Are outside NITS people allowed?",
+    answer:
+      "People from outside NITs are allowed to participate. Eligible students can register for the upcoming conference with a nominal fee.",
+  },
+  {
+    question: "What is the dress code for Youth Parliament?",
+    answer:
+      "The dress code for Youth Parliament is preferably Indian traditionals.",
+  },
+  {
+    question: "Will the Youth Parliament be held online/offline?",
+    answer:
+      "The Youth Parliament will be held offline at the National Institute of Technology, Silchar.",
+  },
+];
+
+export const Eb0 = [
+  {
+    id: 1,
+    post: "** Speaker of the House **",
+    img: "https://res.cloudinary.com/dp92qug2f/image/upload/v1699085104/speaker_ejjihs.jpg",
+    about: (
+      <>
+        After participating in various reputed MUNs, conclaves, conferences and
+        having chaired a few of them,{" "}
+        <a
+          href="https://www.instagram.com/p/CzLQVr1PnMz/"
+          target="_blank"
+          without
+          rel="noreferrer"
+        >
+          <span
+            style={{
+              fontWeight: "900",
+              fontFamily: "Inter",
+              color: "black",
+              textDecoration: "underline",
+            }}
+          >
+            Mr. Ronak Jain
+          </span>
+        </a>{" "}
+        is all set to be the speaker of the Lok Sabha at NITSMUN Youth
+        Parliament 2023. He is a young and passionate leader having an array of
+        interests which primarily includes geo-politics and diplomatic affairs.
+        He is currently pursuing his bachelor’s degree in Computer Science and
+        Engineering at NIT Silchar. Ronak is well recognised for his acerbic
+        debates and arguments.
+      </>
+    ),
+  },
+
+  {
+    id: 2,
+    post: "** Deputy Speaker of the House **",
+    img: "https://res.cloudinary.com/dp92qug2f/image/upload/v1699085104/deputy_e7egsg.jpg",
+    about: (
+      <>
+        Socrates' words, "The more I know, the more I realize I know nothing" is
+        what has always driven {" "}
+        <a
+          href="https://www.instagram.com/p/CzLQLFIPj8x/"
+          target="_blank"
+          without
+          rel="noreferrer"
+        >
+          <span
+            style={{
+              fontWeight: "900",
+              fontFamily: "Inter",
+              color: "black",
+              textDecoration: "underline",
+            }}
+          >
+            George's
+          </span>
+        </a>{" "}
+        belief system. This ingrained convention
+        has always pushed the heights of his curiosity, which has in turn made
+        him win numerous debates and speeches since 2019. Presenting to you Mr
+        George Paul as the Deputy Speaker of the NITSMUN Youth Parliament, 2023.
+        Despite being a littérateur George is again mockingly reckoned as ex-KGB
+        Officer for his dexterous and diplomatic hold over the portfolio of
+        Russian Federation.
+      </>
+    ),
+  },
+];
--- src/components/Pages/YouthParliament/Eb.jsx
+++ src/components/Pages/YouthParliament/Eb.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
-
+import React, { useRef } from "react";
+import './EB.css'
+import { Eb0 } from "./Dataset";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai'
+// import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai'
 const Eb = () => {
+
+  let testimonHolder = useRef();
+
+  function swipeNext() {
+    testimonHolder.current.scrollLeft += (testimonHolder.current.offsetWidth);
+  };
+
+  function swipePrev() {
+    testimonHolder.current.scrollLeft -= (testimonHolder.current.offsetWidth);
+  };
+
+
   return (
-    <div>Eb</div>
+    <>
+      <main id="main__ebb">
+        <div className="mock23-title comt-ipm">
+          <h1>Executive Board</h1>
+        </div>
+        <div className="exe23-ko_p" ref={testimonHolder}>
+          {Eb0.map((item) => {
+            return (
+              <>
+                <div className="eb23-indi__oip" key={item.id}>
+                  <div className="exe23-img__hlder" id="nooverflow__eb" >
+                    <img src={item.img} alt="" />
+                  </div>
+
+
+                  <div className="eb23-indi-abt__pn">
+                    <div className="post-eb23__oip">
+                      <h1> {item.post}</h1>
+                    </div>
+                    <h2>{item.about}</h2>
+                  </div>
+                </div>
+
+
+              </>
+            )
+          })}
+        </div>
+        <div className="btns btnn-hme1 hjip__plk">
+          <button className="prev btn-testimonial" onClick={swipePrev}>
+            <AiOutlineArrowLeft className="btn-indi-hme" />
+          </button>
+          <button className="next btn-testimonial" onClick={swipeNext}>
+            <AiOutlineArrowRight className="btn-indi-hme" />
+          </button>
+        </div>
+      </main>
+    </>
   )
 }
 
